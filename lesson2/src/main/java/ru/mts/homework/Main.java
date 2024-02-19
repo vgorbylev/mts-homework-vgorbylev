48b1fdbd4198435289772dd46163dfe5dfb92731
vgorbylev <vgorbylev@mtsbank.ru>
2024-02-19 18:39:10 +0300

    feature commit

--- lesson2/src/main/java/ru/mts/homework/Main.java
+++ lesson2/src/main/java/ru/mts/homework/Main.java
@@ -2,6 +2,6 @@ package ru.mts.homework;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Earth!");
+        System.out.println("Hello my Earth!");
     }
 }
